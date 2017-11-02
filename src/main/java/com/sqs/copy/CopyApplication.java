aa14307af1e8099ea0fed38bffcac3ff5dfb5fee
Flavio Marcheni <flaviomarcheni@gmail.com>
2017-11-01 22:02:16 -0200

    init commit

---
+++ src/main/java/com/sqs/copy/CopyApplication.java
@@ -0,0 +1,12 @@
+package com.sqs.copy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopyApplication.class, args);
+	}
+}
